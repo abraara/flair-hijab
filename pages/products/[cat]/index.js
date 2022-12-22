1807bb8fefc2c350aef35e1ffb3bdc921469f519
abraara <aahmed98@nait.ca>
2022-12-22 00:53:15 -0700

    workinggg

--- pages/products/[cat]/index.js
+++ pages/products/[cat]/index.js
@@ -1,7 +1,30 @@
 const ProductsCatPage = () => {
     return( 
+        <div>
         <h1>Product</h1>  
+        </div>
     )
 }
 
 export default ProductsCatPage;
+
+export async function getStaticPath() {
+    const {products_categories} = await import('/data/data.json');
+    const allPaths = products_categories.map((pr) => {
+        return {
+            params: {
+                cat: pr.id.toString(),
+            },
+        };
+    });
+
+    return {
+        path: [
+            {
+                params: {
+                    cat: '123',
+                },
+            },
+        ],
+    };
+}
