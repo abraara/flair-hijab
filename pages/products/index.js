b7a3beafe655f14b528e022926c1184e4fb79598
abraara <aahmed98@nait.ca>
2022-12-22 00:54:46 -0700

    working

--- pages/products/index.js
+++ pages/products/index.js
@@ -1,23 +1,14 @@
 import styles from '../../styles/Home.module.css'
+import Image from 'next/image'
 
-const ProductsPage = () => {
+const ProductsPage = ({data}) => {
     return(
         <main className={styles.main}>
         <div>
             <h1>Products Page</h1>
         <div>
-       <a>
-        <img />
-        <h2>Clothing</h2>
-       </a>
-       <a>
-        <img />
-        <h2>Jewelry</h2>
-       </a>
-       <a>
-        <img />
-        <h2>Hijabs</h2>
-       </a>
+        {data.map((pr) => ( <a key={pr.id} href={`/products/${pr.id}`}> <Image alt={pr.title} width={300} height={300} src={pr.image}/> <h2>{pr.title}</h2><p>{pr.description}</p> </a>))}
+
         </div>
         </div>
         </main>
@@ -25,3 +16,12 @@ const ProductsPage = () => {
 }
 
 export default ProductsPage;
+
+export async function getStaticProps() {
+    const {products_categories} = await import('/data/data.json');
+    return{
+        props:{
+            data: products_categories,
+        },
+    };
+}
