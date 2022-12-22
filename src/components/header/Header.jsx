9fc65219bb9fe841623b4f9947a9197ce5ca5e34
abraara <aahmed98@nait.ca>
2022-12-22 14:35:19 -0700

    styles set up

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,11 +3,12 @@ import Link from 'next/link'
 export const Header = () => {
     return (
         <header>
-        <nav>  
-          <img />
-          <Link href='/'>Home</Link>
-          <Link href='/products'>Products</Link>
-          <Link href='/about-us'>About Us</Link>
+        <nav> 
+        <ul className='py-3 px-5 shadow relative flex justify-center gap-4 items-center'>
+          <li><Link href='/'>Home</Link></li>
+          <li><Link href='/products'>Products</Link></li>
+          <li><Link href='/about-us'>About Us</Link></li>  
+        </ul> 
         </nav>
       </header>
     )
