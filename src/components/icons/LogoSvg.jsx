c9d20afc1a5dc306f16c06cfd664bf5dbe0c12ad
abraara <aahmed98@nait.ca>
2022-12-28 16:52:11 -0700

    working

--- src/components/icons/LogoSvg.jsx
+++ src/components/icons/LogoSvg.jsx
@@ -1,7 +1,7 @@
 const LogoSvg = (props) => (
 
 <svg version="1.0" xmlns="http://www.w3.org/2000/svg"
- width="full" height="80" viewBox="200 0 500.000000 500.000000"
+ width="full" height="80" viewBox="0 0 500.000000 500.000000"
  preserveAspectRatio="xMidYMid meet">
 
 <g transform="translate(0.000000,500.000000) scale(0.100000,-0.100000)"
