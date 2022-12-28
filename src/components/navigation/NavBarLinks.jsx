83214a0b8e2b2ecb570828a782651b08688e3d87
abraara <aahmed98@nait.ca>
2022-12-27 17:39:07 -0700

    working

--- src/components/navigation/NavBarLinks.jsx
+++ src/components/navigation/NavBarLinks.jsx
@@ -4,7 +4,7 @@ import { useRouter } from 'next/router';
 function NavBarLinks() {
     const router = useRouter();
     return ( 
-        <ul className="hidden md:flex gap-10 text-slate-600 w-[300px]">
+        <ul className="hidden md:flex gap-10 text-slate-600 w-[425px]">
           <li className='group transition-all duration-300 ease-in-out'><Link className={`hover:text-pink-200 focus:text-pink-300 bg-left-bottom bg-gradient-to-r from-pink-300 to-pink-300 bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out ${router.pathname == "/" ? "text-pink-300" : "text-gray-600"}`}  href='/'>Home</Link></li>
           <li className='group transition-all duration-300 ease-in-out'><Link className={`hover:text-pink-200 focus:text-pink-300 bg-left-bottom bg-gradient-to-r from-pink-300 to-pink-300 bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out ${router.pathname == "/products" ? "text-pink-300" : "text-gray-600"}`}  href='/products'>Products</Link></li>
           <li className='group transition-all duration-300 ease-in-out'><Link className={`hover:text-pink-200 focus:text-pink-300 bg-left-bottom bg-gradient-to-r from-pink-300 to-pink-300 bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out ${router.pathname == "/about-us" ? "text-pink-300" : "text-gray-600"}`} href='/about-us'>About Us</Link></li>  
