a2b552b68e062327e2dbc54be56d5cf3a7755e7b
abraara <aahmed98@nait.ca>
2022-12-28 14:49:26 -0700

    working

--- src/components/icons/NavLogo.jsx
+++ src/components/icons/NavLogo.jsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 
 const NavLogo = (props) => (
 
-   <Image src="/../public/logo.jpg" width={100} height={100} />
+   <Image src="/logo.jpg" width={100} height={100} />
     
     )
     
