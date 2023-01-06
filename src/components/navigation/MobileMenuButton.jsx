a15a8641854965816c526de0310ead2d5cd70e92
abraara <aahmed98@nait.ca>
2023-01-05 18:36:21 -0700

    working

--- src/components/navigation/MobileMenuButton.jsx
+++ src/components/navigation/MobileMenuButton.jsx
@@ -198,21 +198,21 @@ function MobileMenuButton() {
       </ul>
       <ul className="justify-center flex gap-3 pb-[140px]">
          <li>
-            <Link href="https://www.facebook.com/Flair.hijab" id="facebook" target="_blank">
+            <Link href="https://www.facebook.com/Flair.hijab" id="facebookl" target="_blank">
             <button aria-label="facebook" id="facebook" className="bg-white sticky duration-500 border-2 border-blue-600 w-10 transform hover:-translate-y-3 h-10 text-2xl rounded-full hover:bg-blue-600 hover:text-white text-blue-600 " > 
             <FaFacebookF className="mx-auto"/>
             </button>
             </Link>
          </li>
          <li>
-            <Link href="https://www.instagram.com/flairhijab/?hl=en" id="instagram" target="_blank">
+            <Link href="https://www.instagram.com/flairhijab/?hl=en" id="instagraml" target="_blank">
             <button aria-label="instagram" id="instagram" class=" border-2 hover:border-0 border-pink-500 bg-gradient-to-b text-2xl hover:from-indigo-600 hover:via-pink-600 hover:to-yellow-500 min-w-wull hover:text-white bg-white text-pink-600 w-10 h-10  transform hover:-translate-y-3 rounded-full duration-500 ">
             <FaInstagram className="mx-auto" />
             </button>
             </Link>
          </li>
          <li>
-            <Link href="https://www.tiktok.com/@flairhijab" id="tiktok" target="_blank">
+            <Link href="https://www.tiktok.com/@flairhijab" id="tiktokl" target="_blank">
             <button aria-label="tiktok" id="tiktok" class="bg-white transform hover:-translate-y-3 border-2 w-10 h-10 rounded-full duration-500 text-black border-black hover:bg-black hover:text-white text-2xl">
             <FaTiktok className="mx-auto" />
             </button>
