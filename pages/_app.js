5f4531b58f817743d056227a6dd702084b0f2a52
abraara <aahmed98@nait.ca>
2023-01-11 19:40:10 -0700

    working

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import MainLayout from '../src/components/layout/MainLayout'
 import '../styles/globals.css'
 import Head from 'next/head'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }) {
   return (
@@ -10,6 +11,7 @@ export default function App({ Component, pageProps }) {
         <meta name="description" content="Flair Hijab provides Clothing, Hijabs and Jewelry, located in Edmonton, AB/" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        <Script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v15.0" nonce="Dh6kS2Mj"></Script>
       </Head>
     <MainLayout>
     <Component {...pageProps} />
