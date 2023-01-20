8b3efa703036160d7528ef5053e2c45fceaf04ad
abraara <aahmed98@nait.ca>
2023-01-19 22:25:03 -0700

    working

--- src/components/navigation/NavBar.jsx
+++ src/components/navigation/NavBar.jsx
@@ -77,7 +77,7 @@ function NavBar() {
 
                             <h1>Total: {cart.getTotalCost().toFixed(2)}</h1>
 
-                            <button className="border" variant="success" onClick={checkout} >
+                            <button className="border" onClick={checkout} >
                                 Purchase items!
                             </button>
                         </>
