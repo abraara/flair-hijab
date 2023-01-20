ec0843ce2f1175f5d42cf8c8e1dd1a4276c38671
abraara <aahmed98@nait.ca>
2023-01-19 22:58:14 -0700

    adding all products to stripe

--- src/components/productsStore.js
+++ src/components/productsStore.js
@@ -16,7 +16,61 @@ const productsArray = [
         title: "Kurta Men",
         price: 89.99,
         image: "/mens.jpg"
-    }
+    },
+    {
+        id: "price_1MSDXHBtzMyB8huH1BqjuKOy",
+        title: "Maissori Duppattay",
+        price: 29.99,
+        image: "/products/maissori-duppattay.jpg"
+    },
+    {
+        id: "price_1MSDWyBtzMyB8huHircus9P7",
+        title: "Khadi Net Duppatay",
+        price: 39.99,
+        image: "/products/khadi-net-duppattay.jpg"
+    },
+    {
+        id: "price_1MSDWjBtzMyB8huHqm08twi5",
+        title: "Foil Work Duppatay",
+        price: 19.99,
+        image: "/products/foil-work-duppattay.jpg"
+    },
+    {
+        id: "price_1MSDWRBtzMyB8huHUKKEgQAc",
+        title: "Chiffon Embroidered Duppatay",
+        price: 19.99,
+        image: "/products/chiffon-embroidered-duppatay.jpg"
+    },
+    {
+        id: "price_1MSDW7BtzMyB8huHAIaL5SYZ",
+        title: "pakistani jewelry",
+        price: 29.99,
+        image: "/products/jewelry.jpg"
+    },
+    {
+        id: "price_1MSDVtBtzMyB8huHcBjdVRfq",
+        title: "indian jewelry",
+        price: 29.99,
+        image: "/products/jewelry.jpg"
+    },
+    {
+        id: "price_1MSDUfBtzMyB8huHqB2Oz3H9",
+        title: "navrathan jewelry",
+        price: 29.99,
+        image: "/products/navrathan-jewelry.jpg"
+    },
+    {
+        id: "price_1MSDSJBtzMyB8huH41jfn7aU",
+        title: "Shimmer Hijabs",
+        price: 29.99,
+        image: "/products/shimmer-hijab.jpg"
+    },
+    {
+        id: "price_1MSDRoBtzMyB8huHfpXrXEc7",
+        title: "Double Stretch Jersey Hijabs",
+        price: 29.99,
+        image: "/products/double-stretch-jersey-hijab.jpg"
+    },
 ];
 
 function getProductData(id) {
