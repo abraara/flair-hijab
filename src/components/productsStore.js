70b590e1c3b72d510b7d031278da244df947e7e4
abraara <aahmed98@nait.ca>
2023-01-19 23:02:37 -0700

    working

--- src/components/productsStore.js
+++ src/components/productsStore.js
@@ -1,23 +1,5 @@
 const productsArray = [
     {
-        id: "price_1MQQCdBtzMyB8huH7A82IDl3",
-        title: "Rippled Cotton Hijabs",
-        price: 29.99,
-        image: "/products/rippled-cotton-hijab.jpg"
-    },
-    {
-        id: "price_1MQQBWBtzMyB8huH38Qi7GDt",
-        title: "Hijab Accessories",
-        price: 39.99,
-        image: "/products/hijabs.jpg"
-    },
-    {
-        id: "price_1MQQAsBtzMyB8huHu5QSWSaw",
-        title: "Kurta Men",
-        price: 89.99,
-        image: "/mens.jpg"
-    },
-    {
         id: "price_1MSDXHBtzMyB8huH1BqjuKOy",
         title: "Maissori Duppattay",
         price: 29.99,
@@ -71,6 +53,24 @@ const productsArray = [
         price: 29.99,
         image: "/products/double-stretch-jersey-hijab.jpg"
     },
+    {
+        id: "price_1MQQCdBtzMyB8huH7A82IDl3",
+        title: "Rippled Cotton Hijabs",
+        price: 29.99,
+        image: "/products/rippled-cotton-hijab.jpg"
+    },
+    {
+        id: "price_1MQQBWBtzMyB8huH38Qi7GDt",
+        title: "Hijab Accessories",
+        price: 39.99,
+        image: "/products/hijabs.jpg"
+    },
+    {
+        id: "price_1MQQAsBtzMyB8huHu5QSWSaw",
+        title: "Kurta Men",
+        price: 89.99,
+        image: "/mens.jpg"
+    }
 ];
 
 function getProductData(id) {
